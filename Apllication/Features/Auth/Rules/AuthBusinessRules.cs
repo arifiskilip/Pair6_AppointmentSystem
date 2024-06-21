a18a9464453bde3b84c2d269ace9433b41debd23
Kemal Toygun Yıldız <toyguny@hotmail.com>
2024-06-21 21:16:19 +0300

    Revert "UserEmailCheck Edited"
    This reverts commit dccbc794152671464a9ff39308ff0c8b1fc56b32.

--- Apllication/Features/Auth/Rules/AuthBusinessRules.cs
+++ Apllication/Features/Auth/Rules/AuthBusinessRules.cs
@@ -4,7 +4,7 @@ using Core.Application.Rules;
 using Core.CrossCuttingConcers.Exceptions.Types;
 using Core.Security.Hashing;
 using Domain.Entities;
-using MailKit;
+using System.Threading.Tasks;
 
 namespace Application.Features.Auth.Rules
 {
@@ -39,8 +39,8 @@ namespace Application.Features.Auth.Rules
 
         public async Task<User> UserEmailCheck(string email)
         {
-            var user = await _userRepository.GetAsync(x => x.Email.ToLower() == email.ToLower());
-            if (user == null) throw new BusinessException(AuthMessages.UserNotFound);
+            var user = await _userRepository.GetAsync(x=> x.Email.ToLower() == email);
+            if (user is not null) throw new BusinessException(AuthMessages.UserNotFound);
             return user;
         }
 
@@ -55,7 +55,7 @@ namespace Application.Features.Auth.Rules
         public async Task<string> GetUserEmailAsync(int userId)
         {
             var user = await _userRepository.GetAsync(predicate: x => x.Id == userId);
-            if (user != null)
+            if (user is not null)
             {
                 return user.Email;
             }
@@ -66,7 +66,6 @@ namespace Application.Features.Auth.Rules
         {
             if (user == null) throw new BusinessException(AuthMessages.UserNotFound);
         }
-
         public void IsCurrentPasswordCorrect(User user, string currentPassword)
         {
             var check = HashingHelper.VerifyPasswordHash(currentPassword, user.PasswordHash, user.PasswordSalt);
