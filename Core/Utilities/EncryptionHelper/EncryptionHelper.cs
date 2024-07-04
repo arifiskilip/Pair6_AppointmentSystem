d1d2f9f3e6a8d7f59f404fbc9ec3a729c3074897
Arif İskilip <71379630+arifiskilip@users.noreply.github.com>
2024-07-04 15:41:10 +0300

    Added EncryptionHelper.

---
+++ Core/Utilities/EncryptionHelper/EncryptionHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.Utilities.EncryptionHelper
+{
+    public static class EncryptionHelper
+    {
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("YourKeyHere12345"); // 16 byte key
+        private static readonly byte[] IV = Encoding.UTF8.GetBytes("YourIVHere123456"); // 16 byte IV
+
+        public static string Encrypt(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                return plainText;
+
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var sw = new StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+                    }
+
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                return cipherText;
+
+            var buffer = Convert.FromBase64String(cipherText);
+
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (var ms = new MemoryStream(buffer))
+                {
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var sr = new StreamReader(cs))
+                        {
+                            return sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,12 +1,20 @@
 ﻿using Core.Security.Entitites;
+using Core.Utilities.EncryptionHelper;
 
 namespace Domain.Entities
 {
     public class User : BaseUser
     {
+        private string? _identityNumber;
+
+
         public DateTime BirthDate { get; set; }
         public bool IsEmailVerified { get; set; } = false;
-        public string? IdentityNumber { get; set; }
+        public string? IdentityNumber
+        {
+            get => _identityNumber == null ? null : EncryptionHelper.Decrypt(_identityNumber);
+            set => _identityNumber = value == null ? null : EncryptionHelper.Encrypt(value);
+        }
         public string? ImageUrl { get; set; }
 
         public short GenderId { get; set; }
